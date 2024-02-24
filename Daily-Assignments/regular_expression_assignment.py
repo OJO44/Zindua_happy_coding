8bfc0447f673d9e308a61740668f1fa70da87cff
joseph omondi <ochiengjos99@gmail.com>
2024-02-24 12:41:04 +0300

    regular expression assignment

---
+++ Daily-Assignments/regular_expression_assignment.py
@@ -0,0 +1,27 @@
+import re
+
+queries = """reach us out on yusuf89@gmail.com and obunga254@kenya.com. 
+Call us on  (254) 786-3567 or (+234) 222-5445"""
+
+def extract_phone_numbers(string):
+    phone_number_pattern = r"\(?\d{3}\) \d{3}-\d{4}"
+    phone_numbers = re.findall(phone_number_pattern, string)
+    return phone_numbers
+
+phone_numbers = extract_phone_numbers(queries)
+
+for number in phone_numbers:
+    print(number)
+
+
+#Write a function called extract_email_addresses(string) that takes in a string and returns a list of all the email addresses present in the string.
+def extract_email_addresses(string):
+    email_address_pattern =  r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{3}"
+    email_addresses = re.findall(email_address_pattern, string)
+    return email_addresses
+
+email_addresses = extract_email_addresses(queries)
+
+for address in email_addresses:
+    print(address)
+
