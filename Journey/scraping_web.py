b294e9b25512162ea63b713ddb62368793c836d2
joseph omondi <ochiengjos99@gmail.com>
2024-02-27 05:31:42 +0300

    intro to web scraping

---
+++ Journey/scraping_web.py
@@ -0,0 +1,12 @@
+import requests
+import openpyxl
+from bs4 import BeautifulSoup
+
+response = requests.get ("https://www.centralbank.go.ke/rates/forex-exchange-rates/")
+
+#print(response.status_code)
+
+print(response.content)
+
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup)
